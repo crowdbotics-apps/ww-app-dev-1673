5ae21ecce7ec2fc3cbadb1a5bf2ea26be855cd21
Crowdbotics <team@crowdbotics.com>
2020-02-18 12:21:49 +0000

    Update models

---
+++ users/api/v1/viewsets.py
@@ -0,0 +1,13 @@
+from rest_framework import authentication
+from users.models import Test
+from .serializers import TestSerializer
+from rest_framework import viewsets
+
+
+class TestViewSet(viewsets.ModelViewSet):
+    serializer_class = TestSerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = Test.objects.all()
--- ww_app_dev_1673/wsgi.py
+++ ww_app_dev_1673/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ww_app_dev_1673.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ww_app_dev_1673.settings")
 
 application = get_wsgi_application()
